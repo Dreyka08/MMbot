fbc19dc57a6ce27e968ae18d25daf08bcc3a06ad
Dreyka08 <Dreyka.08@gmail.com>
2025-10-23 15:01:07 +0300

    Version 2.0 alpha work all ohne Choosing

--- Main.py
+++ Main.py
@@ -1,24 +1,61 @@
 import pyautogui
 import cv2 
+import mss
+import mss.tools
 import numpy as np
-import subprocess
-import time
+import pygetwindow as gw
 
-Gift = cv2.imread("imgs/GIFT.png")
-Start = cv2.imread("imgs/Start.png")
-prod = cv2.imread("imgs/Prod.png")
-Home = cv2.imread("imgs/OK.png")
-Camp = cv2.imread("imgs/Campani.png")
-AutoBattle = cv2.imread("imgs/AutoBattle.png")
-GoToBattle = cv2.imread("imgs/InBattle.png")
-NewTurn = cv2.imread("imgs/NewTurn.png")
-continuePlay =  cv2.imread("imgs/Continue.png")
-cross = cv2.imread("imgs/Cross.png")
+# Account
+BBack = cv2.imread("imgs/Back.png")
+BAccount = cv2.imread("imgs/Account.png")
+BEntrAcc = cv2.imread("imgs/EntrAcc.png")
+Bvk = cv2.imread("imgs/VK.png")
+Bvkcon = cv2.imread("imgs/VKcon.png")
+
+# Base
+BHome = cv2.imread("imgs/OK.png")
+BHome2 = cv2.imread("imgs/OK2.png")
+BCross = cv2.imread("imgs/Cross.png")
+BCross2 = cv2.imread("imgs/Cross2.png")
+
+# Campany
+BCamp = cv2.imread("imgs/Campani.png")
+BProd = cv2.imread("imgs/Prod.png")
+BDayStart = cv2.imread("imgs/DayStart.png")
+
+#Gift
+BGift = cv2.imread("imgs/GIFT.png")
+BDGift = cv2.imread("imgs/DayGift.png")
+
+BStart = cv2.imread("imgs/Start.png")
+
+# Battle
+BGoToBattle = cv2.imread("imgs/InBattle.png")
+BAutoBattle = cv2.imread("imgs/AutoBattle.png")
+BContinuePlay =  cv2.imread("imgs/Continue.png")
+
+BNewTurn = cv2.imread("imgs/NewTurn.png")
+
+
+blueStaks = gw.getWindowsWithTitle("BlueStacks App Player")[0]
+blueStaks.moveTo(2400, 100)
+blueStaks.resizeTo(1400, 800)
+blueStaks.minimize()
+blueStaks.restore()
+
+def takeScreen():
+    with mss.mss() as sct:
+        monitors = sct.monitors
+        for i, monitor in enumerate(monitors[1:], 1):  # Пропускаем первый (общий)
+            screenshot = sct.grab(monitor)
+            filename = f"imgs/temp.png"
+        mss.tools.to_png(screenshot.rgb, screenshot.size, output=filename)
+        return cv2.imread("imgs/temp.png")
 
 def searchOnScreen(target, screen):
     result = cv2.matchTemplate(target, screen, cv2.TM_CCOEFF_NORMED)
     locations = []
-    for y, x in zip(*np.where(result >= 0.85)):
+    for y, x in zip(*np.where(result >= 0.6)):
         locations.append((x, y))
     if len(locations) != 0:
         answer = [locations[0][0] + 10, locations[0][1] + 10]
@@ -26,82 +63,73 @@ def searchOnScreen(target, screen):
     else:       
         return []
 
-def takeScreen():
-    screen = pyautogui.screenshot()
-    screen.save("imgs/temp.png")
-    screen = cv2.imread("imgs/temp.png")
-    return screen
-
-def BattleStart():
+def clickOnM(img, t):
     ans = []
     while len(ans) == 0:
-        target = GoToBattle
         screen = takeScreen()
-        ans = searchOnScreen(target, screen)
-    pyautogui.moveTo(ans[0], ans[1])
+        ans = searchOnScreen(img, screen)
+    x, y = pyautogui.position()
+    pyautogui.moveTo(ans[0] + (2000 - t), ans[1] + 20)
     pyautogui.click()
+    pyautogui.moveTo(x, y)
+
+def clickOn(img):
     ans = []
     while len(ans) == 0:
-        target = AutoBattle
         screen = takeScreen()
-        ans = searchOnScreen(target, screen) 
-
-    pyautogui.moveTo(ans[0], ans[1])
+        ans = searchOnScreen(img, screen)
+    x, y = pyautogui.position()
+    pyautogui.moveTo(ans[0] + 2000, ans[1] + 20)
     pyautogui.click()
-    ans = []
-    while len(ans) == 0:
-        target = continuePlay
-        screen = takeScreen()
-        ans = searchOnScreen(target, screen)
-    pyautogui.moveTo(ans[0], ans[1])
-    pyautogui.click()
+    pyautogui.moveTo(x, y)
 
-def MainGame():
-    '''
+def pickbetween(FWay, SWay):
+    ansf = []
+    anss = []
     screen = takeScreen()
-    target = Start
-    ans = searchOnScreen(target, screen)
-    pyautogui.moveTo(ans[0], ans[1])
-    pyautogui.click()
-    pyautogui.click()
-    pyautogui.click()
-    ans = []
-    while len(ans) == 0:
-        target = Home
-        screen = takeScreen()
-        ans = searchOnScreen(target, screen)
-    pyautogui.moveTo(ans[0], ans[1])
-    pyautogui.click()
-    time.sleep(2)
-    screen = takeScreen()
-    target = Camp
-    ans = searchOnScreen(target, screen)
-    pyautogui.moveTo(ans[0], ans[1])
-    pyautogui.click()
-    time.sleep(2)
-    screen = takeScreen()
-    target = prod
-    ans = searchOnScreen(target, screen)
-    pyautogui.moveTo(ans[0], ans[1])
-    pyautogui.click()
-    time.sleep(2)
-    pyautogui.moveTo(1000, 305)
-    pyautogui.click()
-    ans = []
-    '''
-    while 1:
-        screen = takeScreen()
+    ansf = searchOnScreen(FWay, screen)
+    anss = searchOnScreen(SWay, screen)
+    if len(ansf) >= len(anss):
+        return 1
+    else:
+        return 2
 
-        target = NewTurn
-        target2 = GoToBattle
-    
-        ans = searchOnScreen(target, screen)
-        ans2 = searchOnScreen(target2, screen)
-        if len(ans2) != 0:
-            BattleStart()
-            
+def RestoreAccount():
+    clickOn(BBack)
+    clickOn(BAccount)
+    clickOn(BEntrAcc)
+    clickOn(Bvk)
+    clickOn(Bvkcon)
+    clickOn(BHome)
+    clickOnM(BCross, 100)
+
+def EnterCompany():
+    clickOn(BCamp)
+    clickOn(BProd)
+    clickOn(BDayStart)
+
+def FirstEnterPerDay():
+    clickOn(BHome2)
+    clickOn(BHome2)
+    EnterCompany()
+    clickOnM(BGift, 30)
+    clickOn(BDGift)
+    clickOn(BHome)
+    clickOnM(BCross2, 30)
+
+def Battle():
+    clickOn(BGoToBattle)
+    clickOnM(BAutoBattle, 25)
+    clickOn(BContinuePlay)
+
+def Main():
+    # RestoreAccount()
+    # FirstEnterPerDay()
+    while True:
+        check = pickbetween(BGoToBattle, BNewTurn)
+        if check == 1:
+            Battle()
         else:
-            pyautogui.moveTo(ans[0], ans[1])
-            pyautogui.click()
-    
-MainGame()
+            clickOn(BNewTurn)
+
+Main()
--- Temp.py
+++ Temp.py
@@ -1,5 +1,9 @@
-import pyautogui
+import pygetwindow as gw
 
-while 1:
-    x, y = pyautogui.position()
-    print(x, " ", y)
+
+blueStaks = gw.getWindowsWithTitle("BlueStacks App Player")[0]
+
+blueStaks.moveTo(2200, 100)
+blueStaks.resizeTo(1000, 800)
+blueStaks.minimize()
+blueStaks.restore()
